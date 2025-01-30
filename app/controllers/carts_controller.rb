8f3d8abea279743b0021c4baa0cca1c921bfe9f6
rsnazario <rafael.s.nazario@gmail.com>
2025-01-30 15:33:35 -0300

    CartsController - V1 - All Routes

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,3 +1,73 @@
 class CartsController < ApplicationController
-  ## TODO Escreva a lógica dos carrinhos aqui
+  before_action :set_cart
+
+  def show
+    render json: cart_response(@cart), status: :ok
+  end
+
+  def create
+    product = Product.find(cart_params[:product_id])
+    cart_product = @cart.cart_products.find_by(product:)
+
+    if cart_product
+      render json: { error: 'Product already exists in the cart!' }, status: :unprocessable_entity
+    else
+      @cart.cart_products.create(cart_params)
+      render json: cart_response(@cart), status: :ok
+    end
+  end
+
+  def add_item
+    product = Product.find(cart_params[:product_id])
+    cart_product = @cart.cart_products.find_by(product:)
+
+    if cart_product
+      cart_product.update(cart_params)
+
+      render json: cart_response(@cart), status: :ok
+    else
+      render json: { error: 'Product should already exist in the cart!' }, status: :unprocessable_entity
+    end
+  end
+
+  def remove_product
+    product = Product.find(cart_params[:product_id])
+    cart_product = @cart.cart_products.find_by(product:)
+
+    if cart_product
+      cart_product.destroy
+      @cart.update_total_price
+
+      render json: { cart: cart_response(@cart), message: 'Product removed from cart' }, status: :ok
+    else
+      render json: { error: 'Product not found in cart' }, status: :not_found
+    end
+  end
+
+  private
+
+  def cart_params
+    params.permit(:product_id, :quantity)
+  end
+
+  def set_cart
+    @cart = session[:cart_id].present? ? Cart.find(session[:cart_id]) : Cart.create!(total_price: 0)
+    session[:cart_id] = @cart.id
+  end
+
+  def cart_response(cart)
+    {
+      id: cart.id,
+      products: cart.cart_products.map do |cp|
+        {
+          id: cp.product.id,
+          name: cp.product.name,
+          quantity: cp.quantity,
+          unit_price: cp.unit_price.to_f,
+          total_price: cp.total_price.to_f
+        }
+      end,
+      total_price: cart.total_price.to_f
+    }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
   resources :products
+  resource :cart do
+    put :add_item, on: :collection
+    delete '/:product_id', to: 'carts#remove_product'
+  end
+
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "rails/health#show"
