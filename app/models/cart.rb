058849475b3425f60a6eee17c357cfb8f5d6669e
rsnazario <rafael.s.nazario@gmail.com>
2025-01-30 15:32:43 -0300

    Add model CartProduct + Addapt related models

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,15 @@
 class Cart < ApplicationRecord
+  # validations
   validates_numericality_of :total_price, greater_than_or_equal_to: 0
 
+  # relations
+  has_many :cart_products, dependent: :destroy
+  has_many :products, through: :cart_products
+
+  # methods
+  def update_total_price
+    self.total_price = cart_products.sum(&:total_price)
+    save!
+  end
   # TODO: lógica para marcar o carrinho como abandonado e remover se abandonado
 end
